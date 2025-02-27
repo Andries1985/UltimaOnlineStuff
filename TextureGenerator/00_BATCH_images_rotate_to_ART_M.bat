56ebac8e2be171132a140e03980fb4c478aed2ec
Sylvash <a.roodbergen@gmail.com>
2025-02-27 21:14:35 +0100

    Minor changes to bat file env var. Added png to bmp3. Need magick as a dependency.

--- TextureGenerator/00_BATCH_images_rotate_to_ART_M.bat
+++ TextureGenerator/00_BATCH_images_rotate_to_ART_M.bat
@@ -1,9 +1,9 @@
 
-echo $PYTHONPATH
+echo %PYTHONPATH%
 ::set PATH=C:\Program Files\Python 3.9;%PATH%
 ::set PYTHONPATH=C:\Program Files\Python 3.9;%PYTHONPATH%
 
-::pip install PIL
+::pip install Pillow
 ::pip install glob
 
 python 00_BATCH_images_rotate_to_ART_M.py
